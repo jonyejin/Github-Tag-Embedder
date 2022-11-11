68561b31e3ae9d9dfd46d4af51523cb252e8892a
nexlice <auspiciouswho47@gmail.com>
2022-11-11 13:28:26 +0900

    done get_test.py

---
+++ sindresorhus/get_text.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = "https://github.com/sindresorhus/awesome"
+response = requests.get(url)
+soup = bs(response.text, 'lxml')
+
+with open('./raw_main.txt', 'w') as file:
+    file.write(soup.get_text())
+
+#nameList = map(bs.get_text, bsObj.findAll("li", {""}))
+
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
