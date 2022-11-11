d90b9129f0617e282c3e621d47a6a21b1dbd7717
nexlice <auspiciouswho47@gmail.com>
2022-11-11 16:07:56 +0900

    refined data

--- sindresorhus/split_by_content.py
+++ sindresorhus/split_by_content.py
@@ -18,26 +18,36 @@ with open('/Users/andrewwonwhoonah/Github-Tag-Embedder/sindresorhus/items_raw.tx
     # 제목 순회
     while True:
         line = f.readline()
-        print(line)
+        # print(line)
         if not line:
             output[content_name] = item_list
-            print(1)
+            # print(1)
             break
         elif line == content_list[index] + "\n":
             content_name = content_list[index]
-            print(2)
+            # print(2)
             continue
         elif index < len(content_list) - 1 and line == content_list[index + 1] + "\n":
             output[content_name] = item_list
             item_list = []
             index += 1
             content_name = content_list[index]
-            print(3)
+            # print(3)
         else:
             item_list.append(line)
-            print(4)
+            # print(4)
             continue
-       
+
+for key in output:
+    for i in range(len(output[key])):
+        element = output[key][i]
+        tmp = element.split('-', 1)
+        # print(tmp)
+        # print(len(tmp))
+        if len(tmp) == 1:
+            output[key][i] = {"sub_content" : tmp[0]}
+        else:
+            output[key][i] = {"sub_content" : tmp[0], "description": tmp[1]}
 
 with open("/Users/andrewwonwhoonah/Github-Tag-Embedder/sindresorhus/output.json", "w") as json_file:
     json.dump(output, json_file)
--- sindresorhus/test.py
+++ sindresorhus/test.py
@@ -1,5 +1,16 @@
-output = {}
+str1 = "Node.js - Async non-blocking event-driven JavaScript runtime built on Chrome's V8 JavaScript engine.\n"
+str2 ="Frontend Development\n"
+output = {"platforms": [str1, str2]}
 
-output["123"] = 123
+for key in output:
+    for i in range(len(output[key])):
+        element = output[key][i]
+        tmp = element.split('-', 1)
+        print(tmp)
+        print(len(tmp))
+        if len(tmp) == 1:
+            output[key][i] = {"sub_content" : tmp[0]}
+        else:
+            output[key][i] = {"sub_content" : tmp[0], "description": tmp[1]}
 
 print(output)
