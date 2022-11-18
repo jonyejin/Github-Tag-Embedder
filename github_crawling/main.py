422afcb065bb1bf79dbb25d14ae7315785028440
jonyejin <i3pivigui@icloud.com>
2022-11-18 20:45:03 +0900

    changed to url array

--- github_crawling/main.py
+++ github_crawling/main.py
@@ -4,18 +4,29 @@ import json
 import urllib.request
 import requests as req
 
-token = "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"
+
+# token = "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" # yejin
+token = "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"
 headers = {"Authorization": "token " + token,
                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko)"
                              "Chrome/84.0.4147.89 Safari/537.36"}
 my_json = {}
+urls = [
+  "https://github.com/keras-team/keras",
+  "https://github.com/scikit-learn/scikit-learn",
+  "https://github.com/apache/superset",
+  "https://github.com/microsoft/ML-For-Beginners",
+  "https://github.com/pandas-dev/pandas",
+]
 
 # yejin: id와 repo_name을 넣으면 파싱된 결과를 리턴해줌
-def whole_func(repo_url, headers):
-    url_dict = {"login": repo_url.split("/")[3], "repo_name": repo_url.split("/")[4]}
-    topics = get_repository_infos(url_dict["login"], url_dict["repo_name"], headers)
-    get_repository_README(url_dict["login"], url_dict["repo_name"], topics)
-
+def whole_func(repo_urls, headers):
+    for repo_url in repo_urls:
+      url_dict = {"login": repo_url.split("/")[3], "repo_name": repo_url.split("/")[4]}
+      topics = get_repository_infos(url_dict["login"], url_dict["repo_name"], headers)
+      get_repository_README(url_dict["login"], url_dict["repo_name"], topics)
+      with open('READMEs.json', 'w', encoding='utf-8') as make_file:
+          json.dump(my_json, make_file, indent="\t")
 
 def get_repository_infos(login, repo_name, headers):
     base_url = f"https://api.github.com/repos/{login}/{repo_name}"
@@ -40,8 +51,6 @@ def get_repository_README(login, repo_name, topics):
         body = f.read().decode('utf-8')
         for t in topics:
           my_json[f"{t}"] = body
-        with open('READMEs.json', 'w', encoding='utf-8') as make_file:
-          json.dump(my_json, make_file, indent="\t")
 
     elif test_r2.status_code == 200:
       print(r2)
@@ -49,8 +58,6 @@ def get_repository_README(login, repo_name, topics):
         body = f.read().decode('utf-8')
         for t in topics:
           my_json[f"{t}"] = body
-        with open('READMEs.json', 'w', encoding='utf-8') as make_file:
-          json.dump(my_json, make_file, indent="\t")
     else:
       print("parse 실패")
-whole_func("https://github.com/keras-team/keras", headers)     
+whole_func(urls, headers)     
