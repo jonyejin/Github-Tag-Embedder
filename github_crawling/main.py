19d4501d7e3874a21bc17b92114a066b90797f5b
jonyejin <i3pivigui@icloud.com>
2022-11-28 20:18:44 +0900

    final github_crawling: 각 keyword를 사용했더니 json에서 덮어씌우는 부작용이 있어서 key를 space로 구분해서 넣음

--- github_crawling/main.py
+++ github_crawling/main.py
@@ -5,8 +5,10 @@ import urllib.request
 import requests as req
 
 
-# token = "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" # yejin
-token = "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"
+# token = "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" #d원후
+# token = "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" # 치현
+# "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" # yejin
+token = "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"
 headers = {"Authorization": "token " + token,
                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko)"
                              "Chrome/84.0.4147.89 Safari/537.36"}
@@ -17,6 +19,20 @@ urls = [
   "https://github.com/apache/superset",
   "https://github.com/microsoft/ML-For-Beginners",
   "https://github.com/pandas-dev/pandas",
+  "https://github.com/GokuMohandas/Made-With-ML",
+  "https://github.com/explosion/spaCy",
+  "https://github.com/donnemartin/data-science-ipython-notebooks",
+  "https://github.com/AMAI-GmbH/AI-Expert-Roadmap",
+  "https://github.com/ray-project/ray",
+  "https://github.com/eugeneyan/applied-ml", # parse 실
+  "https://github.com/eriklindernoren/ML-From-Scratch",
+  "https://github.com/streamlit/streamlit",
+  "https://github.com/Lightning-AI/lightning",
+  "https://github.com/academic/awesome-datascience",
+  "https://github.com/plotly/dash",
+  "https://github.com/fastai/fastbook",
+  "https://github.com/microsoft/Data-Science-For-Beginners",
+  "https://github.com/matplotlib/matplotlib"
 ]
 
 # yejin: id와 repo_name을 넣으면 파싱된 결과를 리턴해줌
@@ -35,7 +51,6 @@ def get_repository_infos(login, repo_name, headers):
     res.raise_for_status()
 
     data = res.json()
-    pages = 400 if data["stargazers_count"] // 100 + 1 > 400 else data["stargazers_count"] // 100 + 1
     # print(data["topics"])
     return data["topics"]
 
@@ -49,15 +64,13 @@ def get_repository_README(login, repo_name, topics):
       print(r1)
       with urllib.request.urlopen(r1) as f:
         body = f.read().decode('utf-8')
-        for t in topics:
-          my_json[f"{t}"] = body
+        my_json[" ".join(topics)] = body
 
     elif test_r2.status_code == 200:
       print(r2)
       with urllib.request.urlopen(r2) as f:
         body = f.read().decode('utf-8')
-        for t in topics:
-          my_json[f"{t}"] = body
+        my_json[" ".join(topics)] = body
     else:
       print("parse 실패")
 whole_func(urls, headers)     
